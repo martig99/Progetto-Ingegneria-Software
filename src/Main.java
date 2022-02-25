e891c77fcee3fd02aaf8e687583d51b67398f35d
martig99 <martina.gualtieri1@gmail.com>
2022-02-25 20:10:41 +0100

    Connection to database

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
+	
+	private static final String DBURL = "jdbc:mysql://db4free.net:3306/circolovelico?";
+	private static final String ARGS = "serverTimezone=UTC";
+	private static final String LOGIN = "admincircolo";
+	private static final String PASSWORD = "Melograno$";
 
 	public static void main(String[] args) {
 		
 		System.out.println("Progetto Finale - Circolo Velico");
+		
+		try (Connection conn = DriverManager.getConnection(DBURL + ARGS, LOGIN, PASSWORD);
+				Statement stmt = conn.createStatement()){
+			System.out.println("Connessione al dabatase avvenuta con successo.");			
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
 
 	}
 
